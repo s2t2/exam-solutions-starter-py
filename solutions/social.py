480fce574795e70c587d48657e2f0682fc28d6ea
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-18 17:29:14 -0400

    Answer question B

--- solutions/social.py
+++ solutions/social.py
@@ -60,6 +60,11 @@ if __name__ == "__main__":
     # ... "print" the screen name of the user who authored that tweet,
     # ... each on a separate line (i.e. "person2", then "person3"):
 
+    print("QUESTION B..")
+    for tweet in tweets:
+        if "@sandwhoa" in tweet["full_text"]:
+            print(tweet["user"]["screen_name"])
+
 
     #
     # QUESTION C
