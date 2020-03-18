9df6a0db9adddd049feb930acf2a63f17aa43a80
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-18 17:35:05 -0400

    Tweets (#2)
    * Answer Question A
    
    * Answer question B

--- solutions/social.py
+++ solutions/social.py
@@ -43,16 +43,15 @@ if __name__ == "__main__":
     print("PROCESSING SOCIAL MEDIA DATA...")
     print("------------------")
     print(tweets)
-
-    # breakpoint()
-
+    #breakpoint()
 
     #
     # QUESTION A
     #
     # "Print" the screen name of the user who authored the first tweet (i.e. "sandwhoa"):
 
-    print("HELLO")
+    print("QUESTION A..")
+    print(tweets[0]["user"]["screen_name"])
 
     #
     # QUESTION B
@@ -60,6 +59,11 @@ if __name__ == "__main__":
     # Of all the tweets which include the phrase "@sandwhoa" in their full text,
     # ... "print" the screen name of the user who authored that tweet,
     # ... each on a separate line (i.e. "person2", then "person3"):
+
+    print("QUESTION B..")
+    for tweet in tweets:
+        if "@sandwhoa" in tweet["full_text"]:
+            print(tweet["user"]["screen_name"])
 
 
     #
